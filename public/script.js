4d284772aabdecd90469ba178b56e919e7b755b4
PalomaC26 <carvalhodasilvapaloma133@gmail.com>
2024-12-12 10:47:04 -0300

    iniciando

--- public/script.js
+++ public/script.js
@@ -219,3 +219,31 @@ window.onclick = function(event) {
         fecharModal();
     }
 };
+
+//----------------------------contato--------------------------//
+// Mostrar o modal de contato
+// Obter o modal
+var modal = document.getElementById("modal-contato");
+
+// Obter o botão que abre o modal
+var btn = document.getElementById("btn-contato");
+
+// Obter o botão de fechar
+var span = document.getElementById("fechar-modal");
+
+// Quando o usuário clicar no botão, abrir o modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Quando o usuário clicar no botão de fechar, fechar o modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Quando o usuário clicar fora do modal, fechá-lo
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
